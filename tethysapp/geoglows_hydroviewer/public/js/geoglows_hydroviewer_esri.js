f8e8abbef85ccf4305096c5a09960e9065d540f3
Riley Hales <rileyhales1@gmail.com>
2022-03-12 15:50:27 -0700

    esit html/js controls for map animation length

--- tethysapp/geoglows_hydroviewer/public/js/geoglows_hydroviewer_esri.js
+++ tethysapp/geoglows_hydroviewer/public/js/geoglows_hydroviewer_esri.js
@@ -3,8 +3,8 @@ mapObj.getPane("watershedlayers").style.zIndex = 250
 mapObj.createPane("viirs")
 mapObj.getPane("viirs").style.zIndex = 200
 // add the legends box to the map
-let legend = L.control({ position: "bottomright" })
-legend.onAdd = function() {
+let legend = L.control({position: "bottomright"})
+legend.onAdd = function () {
     let div = L.DomUtil.create("div", "legend")
     let start =
         '<div><svg width="20" height="20" viewPort="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg">'
@@ -31,15 +31,13 @@ layerAnimationTime.setUTCSeconds(0)
 layerAnimationTime.setUTCMilliseconds(0)
 const currentDate = $("#current-map-date")
 const startDateTime = new Date(layerAnimationTime)
-const endDateTime = new Date(layerAnimationTime.setUTCHours(5 * 24))
+const endDateTime = new Date(layerAnimationTime.setUTCHours(15 * 24))
 layerAnimationTime = new Date(startDateTime)
 currentDate.html(layerAnimationTime)
 const slider = $("#time-slider")
-slider.change(function() {
-    refreshLayerAnimation()
-})
+slider.change(() => refreshLayerAnimation())
 
-function refreshLayerAnimation() {
+const refreshLayerAnimation = () => {
     layerAnimationTime = new Date(startDateTime)
     layerAnimationTime.setUTCHours(slider.val() * 3)
     currentDate.html(layerAnimationTime)
@@ -48,7 +46,7 @@ function refreshLayerAnimation() {
 
 let animate = false
 
-function playAnimation(once = false) {
+const playAnimation = (once = false) => {
     if (!animate) {
         return
     }
@@ -65,22 +63,22 @@ function playAnimation(once = false) {
     setTimeout(playAnimation, 750)
 }
 
-$("#animationPlay").click(function() {
+$("#animationPlay").click(function () {
     animate = true
     playAnimation()
 })
-$("#animationStop").click(function() {
+$("#animationStop").click(function () {
     animate = false
 })
-$("#animationPlus1").click(function() {
+$("#animationPlus1").click(function () {
     animate = true
     playAnimation(true)
 })
-$("#animationBack1").click(function() {
+$("#animationBack1").click(function () {
     if (layerAnimationTime > startDateTime) {
         slider.val(Number(slider.val()) - 1)
     } else {
-        slider.val(40)
+        slider.val((24 / 3) * 15)
     }
     refreshLayerAnimation()
 })
@@ -95,8 +93,7 @@ let VIIRSlayer = L.tileLayer(
 )
 const globalLayer = L.esri
     .dynamicMapLayer({
-        url:
-            "https://livefeeds2.arcgis.com/arcgis/rest/services/GEOGLOWS/GlobalWaterModel_Medium/MapServer",
+        url: "https://livefeeds2.arcgis.com/arcgis/rest/services/GEOGLOWS/GlobalWaterModel_Medium/MapServer",
         useCors: false,
         layers: [0],
         from: startDateTime,
@@ -111,11 +108,11 @@ L.control
             "Gauge Network": gaugeNetwork,
             "VIIRS Imagery": VIIRSlayer
         },
-        { collapsed: false }
+        {collapsed: false}
     )
     .addTo(mapObj)
 
-mapObj.on("click", function(event) {
+mapObj.on("click", function (event) {
     if (mapObj.getZoom() <= 9.5) {
         mapObj.flyTo(event.latlng, 10);
         return
@@ -142,7 +139,7 @@ mapObj.on("click", function(event) {
         .tolerance(10) // map pixels to buffer search point
         .precision(3) // decimals in the returned coordinate pairs
         // .returnGeometry(false)  // include geojson geometry
-        .run(function(error, featureCollection) {
+        .run(function (error, featureCollection) {
             if (error) {
                 updateStatusIcons("fail")
                 alert("Error finding the reach_id")
